28bbf90ca7ac1ca0480c3a009131393a084afe3e
Maxim Schmidt <maxim.schmidt@tum.de>
2023-05-14 08:41:44 +0200

    mock

--- app/network/page.tsx
+++ app/network/page.tsx
@@ -1,9 +1,18 @@
 "use client"
 
+import Image from 'next/image'
+
+import NetworkInitial from '@/public/network/initial.svg'
+import NetworkTransfer from '@/public/network/transfer.svg'
+import NetworkFinal from '@/public/network/final.svg'
+
 export default function Network() {
   return (
     <>
       <h1>Network</h1>
+      <Image src={NetworkInitial} alt="network initial" />
+      <Image src={NetworkTransfer} alt="network transfer" />
+      <Image src={NetworkFinal} alt="network final" />
     </>
   )
 }
--- app/transfer/page.tsx
+++ app/transfer/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import Image from 'next/image'
+
+import TransferMock from '@/public/transfer_mock.svg'
+
 export default function Home() {
   return (
     <>
       <h1>Transfer</h1>
+      <Image src={TransferMock} alt="transfer" />
     </>
   )
 }
