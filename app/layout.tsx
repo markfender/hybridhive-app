e9df89ba6628befc009d91bd9fd4f21d95716c91
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-06-08 17:24:11 +0300

    WIP: Add sidebar & Home page responsiveness for Mobile S

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,49 @@
-"use client"
+"use client";
 
-import './globals.css'
+import "./globals.css";
 
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
 import { ConnectWallet, ThirdwebProvider } from "@thirdweb-dev/react";
 
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import HHLogo from '@/public/hhlogo.svg'
+import HHLogo from "@/public/hhlogo.svg";
 //import HHLogo from '@/public/hybridhive-logo.png'
 
-import { ArrowsRightLeftIcon, HomeIcon, UserCircleIcon, ShareIcon } from '@heroicons/react/20/solid'
+import {
+  ArrowsRightLeftIcon,
+  HomeIcon,
+  UserCircleIcon,
+  ShareIcon,
+} from "@heroicons/react/20/solid";
 
-import { Goerli, Gnosis } from '@thirdweb-dev/chains'
+import { useState } from "react";
+import { Goerli, Gnosis } from "@thirdweb-dev/chains";
 import Wrapper from "./wrapper";
+import { timeStamp } from "console";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const metadata = {
-  title: 'HybridHive App',
-  description: 'Manage accounts and send tokens through HybridHive',
-}
+  title: "HybridHive App",
+  description: "Manage accounts and send tokens through HybridHive",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const [collapsed, setSidebarCollapsed] = useState(true);
+
+  const switchSidebar = () => {
+    console.log("sidebar was switched");
+    setSidebarCollapsed(!collapsed);
+  };
+  // const closeSidebar = () => {}
+
   return (
     <html lang="en">
       <head>
@@ -65,41 +80,75 @@ export default function RootLayout({
           autoConnect={false}
         >
           <div className="flex flex-col flex-start min-h-screen">
-            <header className="flex flex-row p-[15px] bg-[#151515] grow-0">
+            <header className="flex flex-row p-[16px] bg-[#151515] grow-0">
               <div className="flex flex-row justify-between align-center w-[100%]">
                 <div className="flex flex-row items-center">
-                  <Image src={HHLogo} alt="" className="mr-4 w-[48px]"/>
+                  <Image src={HHLogo} alt="" className="mr-4 w-[32px]" />
                   <h1 className="text-[0px] text-white">hybridhive</h1>
                 </div>
                 <ConnectWallet className="connect-wallet-button" />
               </div>
             </header>
-            <nav className="flex flex-row bg-[#FFD12E] p-[15px] grow-0 landscape:justify-center">
-              <div className="flex flex-row justify-between w-[100%] text-white nav-bar text-[0px] landscape:text-sm sm:w-3/4">
-                <Link href="/">
-                  <HomeIcon className="landscape:mr-[10px]" width={25} height={25} />
+            <nav
+              className={`flex flex-col h-screen fixed z-[99] top-0 left-0 bg-[#FFD12E] p-[15px] ${
+                collapsed ? "hidden" : "w-[60%]"
+              }`}
+            >
+              <div className="flex flex-col h-full text-white nav-bar text-[16px]">
+                <Link onClick={() => setSidebarCollapsed(true)} href="/">
+                  <HomeIcon className="mr-[10px]" width={25} height={25} />
                   Home
                 </Link>
-                <Link href="/account">
-                  <UserCircleIcon className="landscape:mr-[10px]" width={25} height={25} />
+                <Link onClick={() => setSidebarCollapsed(true)} href="/account">
+                  <UserCircleIcon
+                    className="mr-[10px]"
+                    width={25}
+                    height={25}
+                  />
                   Account
                 </Link>
-                <Link href="/transfer">
-                  <ArrowsRightLeftIcon className="landscape:mr-[10px]" width={25} height={25} />
+                <Link
+                  onClick={() => setSidebarCollapsed(true)}
+                  href="/transfer"
+                >
+                  <ArrowsRightLeftIcon
+                    className="mr-[10px]"
+                    width={25}
+                    height={25}
+                  />
                   Transfer
                 </Link>
-                <Link href="/network">
-                  <ShareIcon className="rotate-90 landscape:mr-[10px]" width={25} height={25} />
+                <Link onClick={() => setSidebarCollapsed(true)} href="/network">
+                  <ShareIcon
+                    className="rotate-90 mr-[10px]"
+                    width={25}
+                    height={25}
+                  />
                   Network
                 </Link>
               </div>
             </nav>
-            <section className="flex flex-row justify-center p-[50px] grow">
-              <div className="main-section flex flex-col items-start w-3/4 max-w-screen-lg">
+            {collapsed ? (
+              <button
+                onClick={switchSidebar}
+                className="flex justify-center items-center w-[52px] h-[52px] bg-[#FFD12E] rounded-full fixed bottom-[30px] right-[40px] z-[99] text-[20px] font-bold"
+              >
+                &#9776;
+              </button>
+            ) : (
+              <button
+                onClick={switchSidebar}
+                className="flex justify-center items-center w-[52px] h-[52px] leading-[52px] bg-[#FFD12E] rounded-full fixed bottom-[30px] right-[40px] z-[99] text-[30px]"
+              >
+                &times;
+              </button>
+            )}
+            <section className="flex flex-row justify-center p-[15px] grow">
+              <div className="main-section flex flex-col items-start">
                 <Wrapper>{children}</Wrapper>
               </div>
             </section>
-            <footer className="flex flex-row justify-center bg-[#EEEEEE] p-[15px] grow-0">
+            <footer className="flex flex-row justify-center bg-[#EEEEEE] py-[5px] grow-0">
               <p className="text-lg text-[#222]">© 2023 hybridhive</p>
             </footer>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Start() {
   return (
     <>
       <h1>HybridHive</h1>
-      <p>a protocol to combine the benefits of single and multi-currency systems</p>
+      <p className="mb-[15px]">a protocol to combine the benefits of single and multi-currency systems</p>
       <div className="max-w-md mx-auto">
         <h2>Search network</h2>
-        <div className="mb-4">
+        <div className="mb-1">
             <input type="text" id="searchInput" className="w-full px-4 py-2 rounded-lg border border-gray-300" placeholder="Enter your search query" />
         </div>
         <div className="mb-4">
-            <button id="searchButton" className="px-4 py-2 rounded-lg bg-blue-500 text-white">Check network</button>
+            <button id="searchButton" className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white">Check network</button>
         </div>
 
         <h2>Added networks</h2>
