0a0c40b7564f4d4f4779d7a704beef6f61be84da
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-06-13 17:09:45 +0300

    feat: remove hybridhive text from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,30 @@
-"use client"
+"use client";
 
 export default function Start() {
   return (
     <>
-      <h1>HybridHive</h1>
-      <p className="mb-[15px]">a protocol to combine the benefits of single and multi-currency systems</p>
       <div className="max-w-md mx-auto">
-        <h2>Search network</h2>
+        <h6>Search network</h6>
         <div className="mb-1">
-            <input type="text" id="searchInput" className="w-full px-4 py-2 rounded-lg border border-gray-300" placeholder="Enter your search query" />
+          <input
+            type="text"
+            id="searchInput"
+            className="w-full px-4 py-2 rounded-lg border border-gray-300"
+            placeholder="Enter your search query"
+          />
         </div>
         <div className="mb-4">
-            <button id="searchButton" className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white">Check network</button>
+          <button
+            id="searchButton"
+            className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white"
+          >
+            Check network
+          </button>
         </div>
 
-        <h2>Added networks</h2>
-        <div id="searchResults" className="bg-white shadow">
-        </div>
+        <h3>Added networks</h3>
+        <div id="searchResults" className="bg-white shadow"></div>
       </div>
     </>
-  )
+  );
 }
