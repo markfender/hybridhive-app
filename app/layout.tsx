b9c55a5f0170e2704abad97af70922c30e3c067d
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-06-14 14:46:01 +0300

    fix: fix paddings and margins on landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,17 +81,19 @@ export default function RootLayout({
         >
           <div className="flex flex-col flex-start min-h-screen">
             {/* <header className="flex flex-row p-[16px] bg-[#151515] grow-0"> */}
-            <header className="flex flex-row p-[16px] bg-white grow-0">
+            <header className="flex flex-row p-[16px] lg:px-[32px] bg-white grow-0">
               <div className="flex flex-row justify-between align-center w-[100%]">
                 <div className="flex flex-row items-center">
                   <button
                     onClick={switchSidebar}
-                    className="flex justify-center items-center w-[52px] h-[52px] bg-yellow-300 rounded-full md:rounded-none fixed bottom-[30px] right-[40px] md:relative md:bottom-0 md:right-0 md:bg-transparent md:text-slate-900 z-[98] text-[20px] md:text-[22px] font-bold"
+                    className="hidden flex justify-center items-center w-[52px] h-[52px] bg-yellow-300 rounded-full md:rounded-none fixed bottom-[30px] right-[40px] md:relative md:bottom-0 md:right-0 md:bg-transparent md:text-slate-900 z-[98] text-[20px] md:text-[22px] font-bold"
                   >
                     &#9776;
                   </button>
                   <Image src={HHLogo} alt="" className="mr-4 w-[32px]" />
-                  <h1 className="text-[0px] text-white">hybridhive</h1>
+                  <h1 className=" text-[0] md:text-[2rem]  text-slate-900">
+                    hybridhive
+                  </h1>
                 </div>
                 <ConnectWallet
                   className="connect-wallet-button"
